ba90fff22e62301ed1d26fce00199bfdfaa0e216
eternaldrowsy <91853899+eternaldrowsy@users.noreply.github.com>
2022-04-01 19:28:30 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,5 +5,5 @@ std::cout<<"Input name"<< std::endl;
 std::string name;
 std::cin>>name;
 std::cout <<"Hello world! from @" <<name<< std::endl;
-return 0; //Добавил, коммент
+return 0; //Добавил коммент
 }
