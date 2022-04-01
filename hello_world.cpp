900e8cdc44cc4f8bb1daf32ada02fb9e7b27cff6
eternaldrowsy <a.m.moisenovich@mail.ru>
2022-04-01 18:05:29 +0300

    helloworld without namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-using namespace std;
+#include<string>
 int main(){
-cout <<"Hello world!" << endl;
+std::cout<<"Input name"<< std::endl;
+std::string name;
+std::cin>>name;
+std::cout <<"Hello world! from @" <<name<< std::endl;
 return 0;
 }
